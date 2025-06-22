e027c50a42d7b6da7a3f646283ab1a932785d0e6
Massil <amirat.massil@gmail.com>
2025-06-22 12:18:30 +0200

    rep

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
 
   return (
     <div className="h-screen flex items-center justify-center flex-col space-y-4">
-      <h1 className="text-2xl font-bold">T'AS ACHETE QUOI?????????????????? STP</h1>
+      <h1 className="text-2xl font-bold">T AS ACHETE QUOI?????????????????? STP</h1>
       {/* <button onClick={() => handleSelect('admin')} className="px-4 py-2 bg-pink-500 text-white rounded-full">Je suis elle ❤️</button>
       <button onClick={() => handleSelect('user')} className="px-4 py-2 bg-blue-500 text-white rounded-full">Je suis lui 😎</button> */}
     </div>
